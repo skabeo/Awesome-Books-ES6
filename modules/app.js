3f78cd7b4db628b2c8e2e3d6c2088555038b3310
skabeo <spencerokyere95@gmail.com>
2023-03-27 14:05:58 +0100

    Use ES6 syntax

--- modules/app.js
+++ modules/app.js
@@ -8,20 +8,19 @@ export default function initializePage() {
   navLinks[0].style.color = 'blue';
 
   for (let i = 0; i < navLinks.length; i += 1) {
-    navLinks[i].addEventListener('click', function update(event) {
+    navLinks[i].addEventListener('click', (event) => {
       event.preventDefault();
       for (let j = 0; j < sections.length; j += 1) {
         sections[j].style.display = 'none';
       }
-      const target = this.getAttribute('href').substr(1);
+      const target = navLinks[i].getAttribute('href').substr(1);
       document.getElementById(target).style.display = 'block';
-
       for (let k = 0; k < navLinks.length; k += 1) {
         navLinks[k].style.color = 'black';
       }
-      this.style.color = 'blue';
+      navLinks[i].style.color = 'blue';
     });
-  }
+  }  
 
   sections[0].style.display = 'block';
 }
