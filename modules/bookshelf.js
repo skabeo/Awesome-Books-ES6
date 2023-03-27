9809d5a39fe27b9ada288f78ec0b7dd9676eb5bc
skabeo <spencerokyere95@gmail.com>
2023-03-27 12:17:57 +0100

    Add modules folder

---
+++ modules/bookshelf.js
@@ -0,0 +1,38 @@
+export default class BookShelf {
+  constructor() {
+    this.books = JSON.parse(localStorage.getItem('book')) || [{ title: 'title 1', author: 'author 2' }, { title: 'title 1', author: 'author 2' }];
+  }
+
+  add(title, author) {
+    this.books.push({ title, author });
+    localStorage.setItem('book', JSON.stringify(this.books));
+  }
+
+  remove(book) {
+    this.books = this.books.filter((b) => b !== book);
+    localStorage.setItem('book', JSON.stringify(this.books));
+  }
+
+  render() {
+    const bookList = document.querySelector('#book-list');
+    bookList.innerHTML = '';
+    for (let i = 0; i < this.books.length; i += 1) {
+      const book = this.books[i];
+      const li = document.createElement('li');
+      li.innerHTML = `${book.title} by ${book.author}`;
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Remove';
+      deleteButton.classList.add('deleteButton');
+      deleteButton.addEventListener('click', () => {
+        this.remove(book);
+        this.render();
+      });
+      li.appendChild(deleteButton);
+      bookList.appendChild(li);
+
+      if (i % 2 === 0) {
+        li.style.backgroundColor = '#e9e9e9';
+      }
+    }
+  }
+}
